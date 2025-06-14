5dabcaa3b03f07f118dc7c5475722c84983aae67
sunshineeternal85 <redpanda.python@gmail.com>
2025-06-14 15:16:04 +0100

    update load fmnist

--- Section_18_train_reg.py
+++ Section_18_train_reg.py
@@ -12,7 +12,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
-from torch.utils.data import DataLoader, Dataset, random_split, RandomSampler, ConcatDataset
+from torch.utils.data import DataLoader, Dataset, random_split, Subset, ConcatDataset
 
 from torchvision import transforms
 from torchvision.datasets import FashionMNIST
@@ -49,7 +49,7 @@ def transformer_flip():
         transforms.RandomHorizontalFlip(p=1)
         ]
     )
-    logging.info(f'return flip')
+    logging.info(f'return data with flip')
     return transform
 
 
@@ -63,20 +63,26 @@ def transformer_grey():
         transforms.RandomGrayscale(p=1)
         ]
     )
-    logging.info(f'return flip')
+    logging.info(f'return data with grey')
     return transform
 
 
 
 
-def denormalize(tensor, mean=(0.5,), std=(0.5,)):
+def denormalize(tensor: torch.Tensor, mean=(0.5,), std=(0.5,)):
     """
     Denormalizes a tensor image.
     Assumes the tensor is in [C, H, W] format and values are in [-1.0, 1.0]
     after having been normalized with the given mean and std.
     """
-    mean_tensor = torch.tensor(mean).view(len(mean), 1, 1)
-    std_tensor = torch.tensor(std).view(len(std), 1, 1)
+    logging.debug(f'min: {tensor.min().item()} max: {tensor.max().item()}')
+
+
+    mean_tensor = torch.tensor(mean, device=tensor.device).view(len(mean), 1, 1)
+    std_tensor = torch.tensor(std, device=tensor.device).view(len(std), 1, 1)
+
+    logging.debug(f'mean_tesor shape {mean_tensor.shape}')
+    logging.debug(f'std_tesor shape {std_tensor.shape}')
 
     denormalized_tensor = (tensor * std_tensor) + mean_tensor
 
@@ -90,18 +96,27 @@ def denormalize(tensor, mean=(0.5,), std=(0.5,)):
 
 
 def imshow(image_tensor:torch.Tensor, ax=None):
-    #image_tensor = denormalize(image_tensor)
-    img_np = image_tensor.numpy()
-    if not ax:
 
-        plt.imshow(img_np[0],cmap='grey')
-    plt.show()
+    image_tensor_denorm = denormalize(image_tensor)
+    img_np = image_tensor_denorm.cpu().squeeze().numpy()
+    if ax is None:
+
+        plt.imshow(img_np, cmap='gray')
+        plt.show()
+
+    else:
+        ax.imshow(img_np, cmap='gray')
+        
 
 
 
 # %%
 
 if __name__ == '__main__':
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
     root = '/media/laurent/SSD2/dataset/fmnist'
 
     os.makedirs(root,exist_ok=True)
@@ -109,32 +124,44 @@ if __name__ == '__main__':
     train_dataset = FashionMNIST(root=root,train=True, download=True,transform= transformer())
     test_dataset = FashionMNIST(root=root,train=False, download=True, transform= transformer())
 
+    np.random.seed(123)
+    index_sample = np.random.choice(len(train_dataset), 3000, replace=True)
+
     train_dataset_trans1 = FashionMNIST(root=root,train=True, download=True,transform= transformer_flip())
+    train_dataset_trans1 = Subset(train_dataset_trans1,index_sample)
+
+
     train_dataset_trans2 = FashionMNIST(root=root,train=True, download=True,transform= transformer_grey())
+    train_dataset_trans2 = Subset(train_dataset_trans2,index_sample)
+    
 
     train_dataset = ConcatDataset([train_dataset,
-                                   RandomSampler(data_source=train_dataset_trans1, replacement=True,num_samples=6000),
-                                   RandomSampler(data_source=train_dataset_trans2, replacement=True,num_samples=6000)
+                                   train_dataset_trans1,
+                                   train_dataset_trans2
                                    ])
 
-
-    print(len(train_dataset))
-    logging.info(f'dl train data in {train_dataset}')
-    logging.info(f'dl testt data in {test_dataset}')
+    logging.info(f'train_dataset has {len(train_dataset)} items')
+    logging.info(f'test_dataset has {len(test_dataset)} items')
 
     logging.info(f'x size: {train_dataset[0][0].size()}')
 
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
     test_dataloader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False)
 
+    fig, ax = plt.subplots(3,3, figsize=(4,4))
+ 
+    ax_flatten = ax.flatten()
+
+    num_images_to_plot = min(len(train_dataset), len(ax_flatten))
 
 
-
-    for index, (x, y) in enumerate(train_dataset):
-        logging.info(f'x size: {x.size()}\n')
-
-        if index == 0:
-            break
+    for i in range(num_images_to_plot):
+        x, y = train_dataset[i]
+        imshow(x, ax_flatten[i])
+        ax_flatten[i].set_title(f'Label: {y}') # Add label as title
+        ax_flatten[i].axis('off') 
+    plt.tight_layout()
+    plt.show()
 
 
 
